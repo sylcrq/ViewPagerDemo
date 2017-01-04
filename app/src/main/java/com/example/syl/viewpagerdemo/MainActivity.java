d9331f2997e23f93f91ada5d1d792de0fc07d51b
shenyunlong <shenyunlong@qiyi.com>
2017-01-04 18:42:58 +0800

    Add TestActivity & SettingsActivity MainActivity --startActivityForResult--> TestActivity --startActivity--> SettingsActivity

--- app/src/main/java/com/example/syl/viewpagerdemo/MainActivity.java
+++ app/src/main/java/com/example/syl/viewpagerdemo/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.syl.viewpagerdemo;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.syl.viewpagerdemo.dummy.DummyContent;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener,
         PageFragment.OnListFragmentInteractionListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,17 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnFr
 
     @Override
     public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Toast.makeText(this, "" + item.content, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, TestActivity.class);
+        startActivityForResult(intent, 1024);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "xxxx#" + requestCode + "," + resultCode);
+        if (requestCode == 1024) {
+            Log.d(TAG, "yyy#" + requestCode + "," + resultCode);
+        }
     }
 }
---
+++ app/src/main/java/com/example/syl/viewpagerdemo/TestActivity.java
@@ -0,0 +1,34 @@
+package com.example.syl.viewpagerdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class TestActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+}
